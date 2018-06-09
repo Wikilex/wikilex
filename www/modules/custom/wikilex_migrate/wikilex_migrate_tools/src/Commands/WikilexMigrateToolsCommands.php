1f46f78c3256f110f9157e7ceaff4be390ce76a3
Pierre <contact@pierrenoel.pro>
2018-06-09 19:04:37 +0200

    Ajout d'une commande drush wikilex-migrate-status

--- www/modules/custom/wikilex_migrate/wikilex_migrate_tools/src/Commands/WikilexMigrateToolsCommands.php
+++ www/modules/custom/wikilex_migrate/wikilex_migrate_tools/src/Commands/WikilexMigrateToolsCommands.php
@@ -2,18 +2,25 @@
 
 namespace Drupal\wikilex_migrate_tools\Commands;
 
+use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Drupal\bootstrap\Utility\Unicode;
+use Drupal\Core\Datetime\DateFormatter;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\migrate\Exception\RequirementsException;
+use Drupal\migrate\Plugin\RequirementsInterface;
 use Drupal\migrate_tools\Commands\MigrateToolsCommands;
 use Drupal\migrate_tools\MigrateExecutable;
 use Drupal\migrate\Plugin\MigrationInterface;
 
 /**
  * Drush commandes specialement conçues pour réaliser un import,
- * en passant une option supplémentaire.
+ * ou obtenir un status de migration en passant une option supplémentaire.
  * Cette option permet de rechercher et d'importer par code de lois unique
  * plutot que d'essayer de tous les importer.
  * Nécessite Drush ^9.2
  */
 class WikilexMigrateToolsCommands extends MigrateToolsCommands {
+
 
   /**
    * Perform one or more migration processes.
@@ -156,6 +163,166 @@ class WikilexMigrateToolsCommands extends MigrateToolsCommands {
         )
       );
     }
+  }
+
+  /**
+   * List all migrations with current status.
+   *
+   * @param string $migration_names
+   *   Restrict to a comma-separated list of migrations (Optional).
+   * @param array $options
+   *   Additional options for the command.
+   *
+   * @command migrate:status
+   *
+   * @option cid L'id du code de lois à importer
+   * @option group A comma-separated list of migration groups to list
+   * @option tag Name of the migration tag to list
+   * @option names-only Only return names, not all the details (faster)
+   *
+   * @usage migrate:status
+   *   Retrieve status for all migrations
+   * @usage migrate:status --group=beer
+   *   Retrieve status for all migrations in a given group
+   * @usage migrate:status --tag=user
+   *   Retrieve status for all migrations with a given tag
+   * @usage migrate:status --group=beer --tag=user
+   *   Retrieve status for all migrations in the beer group
+   *   and with the user tag.
+   * @usage migrate:status beer_term,beer_node
+   *   Retrieve status for specific migrations
+   *
+   * @validate-module-enabled migrate_tools
+   *
+   * @aliases wms, wikilex-migrate-status
+   *
+   * @field-labels
+   *   group: Group
+   *   id: Migration ID
+   *   status: Status
+   *   total: Total
+   *   imported: Imported
+   *   unprocessed: Unprocessed
+   *   last_imported: Last Imported
+   * @default-fields group,id,status,total,imported,unprocessed,last_imported
+   *
+   * @return \Consolidation\OutputFormatters\StructuredData\RowsOfFields
+   *   Migrations status formatted as table.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   */
+  public function status(
+    $migration_names = '',
+    array $options = [
+      'cid' => NULL,
+      'group' => NULL,
+      'tag' => NULL,
+      'names-only' => NULL,
+    ]
+  ) {
+    if (!$options['cid']) {
+      throw new \Exception(dt("L'id du code de lois (CID) est obligatoire pour un status de migration wikilex."));
+    }
+
+    $names_only = $options['names-only'];
+
+    $migrations = $this->migrationsList($migration_names, $options);
+
+    $table = [];
+    // Take it one group at a time, listing the migrations within each group.
+    foreach ($migrations as $group_id => $migration_list) {
+      /** @var \Drupal\migrate_plus\Entity\MigrationGroup $group */
+      $group = $this->entityTypeManager->getStorage('migration_group')->load($group_id);
+      $group_name = !empty($group) ? "{$group->label()} ({$group->id()})" : $group_id;
+
+      foreach ($migration_list as $migration_id => $migration) {
+        if ($names_only) {
+          $table[] = [
+            'group' => dt('Group: @name', ['@name' => $group_name]),
+            'id' => $migration_id,
+          ];
+        }
+        else {
+          try {
+            $map = $migration->getIdMap();
+            $imported = $map->importedCount();
+            $source_plugin = $migration->getSourcePlugin();
+
+            // Ajout méthode custom WIKILEX.
+            if (method_exists($source_plugin, 'setCid')) {
+              $source_plugin->setCid($options['cid']);
+            }
+          }
+          catch (\Exception $e) {
+            $this->logger()->error(
+              dt(
+                'Failure retrieving information on @migration: @message',
+                ['@migration' => $migration_id, '@message' => $e->getMessage()]
+              )
+            );
+            continue;
+          }
+
+          try {
+            $source_rows = $source_plugin->count();
+            // -1 indicates uncountable sources.
+            if ($source_rows == -1) {
+              $source_rows = dt('N/A');
+              $unprocessed = dt('N/A');
+            }
+            else {
+              $unprocessed = $source_rows - $map->processedCount();
+            }
+          }
+          catch (\Exception $e) {
+            $this->logger()->error(
+              dt(
+                'Could not retrieve source count from @migration: @message',
+                ['@migration' => $migration_id, '@message' => $e->getMessage()]
+              )
+            );
+            $source_rows = dt('N/A');
+            $unprocessed = dt('N/A');
+          }
+
+          $status = $migration->getStatusLabel();
+          $migrate_last_imported_store = $this->keyValue->get(
+            'migrate_last_imported'
+          );
+          $last_imported = $migrate_last_imported_store->get(
+            $migration->id(),
+            FALSE
+          );
+          if ($last_imported) {
+            $last_imported = $this->dateFormatter->format(
+              $last_imported / 1000,
+              'custom',
+              'Y-m-d H:i:s'
+            );
+          }
+          else {
+            $last_imported = '';
+          }
+          $table[] = [
+            'group' => $group_name,
+            'id' => $migration_id,
+            'status' => $status,
+            'total' => $source_rows,
+            'imported' => $imported,
+            'unprocessed' => $unprocessed,
+            'last_imported' => $last_imported,
+          ];
+        }
+      }
+
+      // Add empty row to separate groups, for readability.
+      end($migrations);
+      if ($group_id !== key($migrations)) {
+        $table[] = [];
+      }
+    }
+
+    return new RowsOfFields($table);
   }
 
 }
